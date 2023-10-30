02454f9ca69320fe446638c77629e407dcb25cad
Amastina1 <nnantaa@gmail.com>
2023-10-30 16:05:41 -0700

    1-run_inline

--- 0x00-python-hello_world/main.py
+++ 0x00-python-hello_world/main.py
@@ -5,3 +5,8 @@ guillaume@ubuntu:~/py/0x00$ export PYFILE=main.py
 guillaume@ubuntu:~/py/0x00$ ./0-run
 Best School
 guillaume@ubuntu:~/py/0x00$
+
+guillaume@ubuntu:~/py/0x00$ export PYCODE='print(f"Best School: {88+10}")'
+guillaume@ubuntu:~/py/0x00$ ./1-run_inline
+Best School: 98
+guillaume@ubuntu:~/py/0x00$
